cd359f98bdc8e4614a248ed528eb528ddbcc6323
Federico Manfredi Longo <federicomanfredi.longo@gmail.com>
2023-02-14 14:23:57 +0100

    store test

---
+++ internal/log/store_test.go
@@ -0,0 +1,132 @@
+package log
+
+/*Test store package*/
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("ciao")
+	width = uint64(len(write)) + lenWidth
+)
+
+/*
+Main test
+
+Creaiamo uno store con un file temporaneo
+Che richiamo due test helper per testare l'aggiunta e la lettura dello store
+Ricrea lo store e viene testato in lettura per essere sicuri che si riavvi al suo stato dopo il ripristino
+*/
+func TestStoreAppendRead(t *testing.T) {
+
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+
+}
+
+func testAppend(t *testing.T, s *store) {
+
+	t.Helper()
+
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+
+	t.Helper()
+
+	var pos uint64
+
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, lenWidth, n)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, write, b)
+		require.Equal(t, int(size), n)
+		off += int64(n)
+
+	}
+
+}
+
+func TestStoreClose(t *testing.T) {
+
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+
+	defer os.Remove(f.Name())
+
+	s, err := newStore()
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(r, err)
+
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+
+}
+
+func openFile(name string) (file *os.File, siez int64, err error) {
+
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, o, err
+	}
+	return f, fi.Size(), nil
+
+}
