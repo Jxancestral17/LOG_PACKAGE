9a7d73e162a524f236a0b7956dbe93e25a17614a
Federico Manfredi Longo <federicomanfredi.longo@gmail.com>
2023-02-14 17:20:29 +0100

    index_test

---
+++ internal/log/index_test.go
@@ -0,0 +1,67 @@
+package log
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+/*
+Creiamo un file indicie e lo rendiamo abbastanza grande da contenere le voci
+
+Il file viene ingradino prima di usarlo cosi da mappare il file in memoria
+su una fetta di byte
+In caso non viene fatto avemmo un errore
+
+
+*/
+
+func TestIndex(t *testing.T) {
+	f, err := ioutil.TempFile(os.TempDir(), "index_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	c := Config{}
+
+	c.Segment.MaxIndexBytes = 1024
+	idx, err := newIndex(f, c)
+	require.NoError(t, err)
+
+	//Verifichiamo di poter rileggere la stessa voce
+	_, _, err = idx.Read(-1)
+	require.Error(t, err)
+	require.Equal(t, f.Name(), idx.Name())
+	entries := []struct {
+		Off uint32
+		Pos uint64
+	}{
+		{Off: 0, Pos: 0},
+		{Off: 1, Pos: 10},
+	}
+
+	for _, want := range entries {
+		err = idx.Write(want.Off, want.Pos)
+		require.NoError(t, err)
+
+		_, pos, err := idx.Read(int64(want.Off))
+		require.NoError(t, err)
+		require.Equal(t, want.Pos, pos)
+	}
+
+	//Verifichiamo indice ed errore dello scannner in fase di lettura oltre il numero di voci indicizzate
+	_, _, err = idx.Read(int64(len(entries)))
+	require.Equal(t, io.EOF, err)
+	_ = idx.Close()
+
+	f, _ = os.OpenFile(f.Name(), os.O_RDWR, 0600)
+	idx, err = newIndex(f, c)
+	require.NoError(t, err)
+	off, pos, err := idx.Read(-1)
+	require.NoError(t, err)
+	require.Equal(t, uint32(1), off)
+	require.Equal(t, entries[1].Pos, pos)
+
+}
